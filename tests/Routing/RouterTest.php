ca42de8b5b859d03b265133a2efccccbe0660ba3
Mat Munn <mat@matmunn.me>
2017-03-22 17:58:38 +1100

    Switched to a couple of stubs for PHP 5.6 validity
    Signed-off-by: Mat Munn <mat@matmunn.me>

--- tests/Routing/RouterTest.php
+++ tests/Routing/RouterTest.php
@@ -1,20 +1,16 @@
 <?php
 
+use Bedrock\Routing\Router;
 use Bedrock\Testing\TestCase;
+use Bedrock\Testing\Stubs\RouteDoesntExistStub;
+use Bedrock\Testing\Stubs\MethodDoesntExistStub;
 
 class RouterTest extends TestCase
 {
     /** @test */
     function an_exception_is_thrown_if_the_method_doesnt_exist()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->put('/test-route', function () {
-                    return 'test response';
-                });
-            }
-        };
+        $this->router = new Bedrock\Routing\Router;
 
         try {
             $this->post('/test-route')
@@ -29,9 +25,8 @@ class RouterTest extends TestCase
     /** @test */
     function an_exception_is_thrown_if_the_same_route_is_added_twice()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
+        $this->router = new Bedrock\Routing\Router;
 
-        };
         try {
             $this->router->put('/test-route', function () {
                 return 'test response';
@@ -49,14 +44,7 @@ class RouterTest extends TestCase
     /** @test */
     function an_exception_is_thrown_if_the_route_doesnt_exist()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->get('/', function () {
-                    return 'something';
-                });
-            }
-        };
+        $this->router = new RouteDoesntExistStub;
         try {
             $this->get('/test-things')
                 ->executeRequest();
@@ -70,14 +58,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_perform_a_get_request()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->get('/', function () {
-                    return 'test response';
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->get('/', function () {
+            return 'test response';
+        });
 
         $this->get('/')
             ->see('test response');
@@ -86,14 +70,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_perform_a_post_request()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->post('/', function () {
-                    return 'test response';
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->post('/', function () {
+            return 'test response';
+        });
 
         $this->post('/')
             ->see('test response');
@@ -102,14 +82,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_perform_a_put_request()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->put('/test-route', function () {
-                    return 'test response';
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->put('/test-route', function () {
+            return 'test response';
+        });
 
         $this->put('/test-route')
             ->see('test response');
@@ -118,14 +94,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_perform_a_patch_request()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->patch('/test-route', function () {
-                    return 'test response';
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->patch('/test-route', function () {
+            return 'test response';
+        });
 
         $this->patch('/test-route')
             ->see('test response');
@@ -134,14 +106,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_perform_a_delete_request()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->delete('/route', function () {
-                    return 'test response';
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->delete('/route', function () {
+            return 'test response';
+        });
 
         $this->delete('/route')
             ->see('test response');
@@ -150,14 +118,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_call_a_route_with_a_parameter()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->get('/test/{number}/something', function ($number) {
-                    return "You entered $number";
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->get('/test/{number}/something', function ($number) {
+            return "You entered $number";
+        });
 
         $this->get('/test/2/something')
             ->see("You entered 2");
@@ -166,14 +130,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_call_a_route_with_multiple_parameters()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->get('/test/{word}/something/{data}', function ($word, $data) {
-                    return "You entered $word and $data";
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->get('/test/{word}/something/{data}', function ($word, $data) {
+            return "You entered $word and $data";
+        });
 
         $this->get('/test/foo/something/bar')
             ->see("You entered foo and bar");
@@ -182,14 +142,10 @@ class RouterTest extends TestCase
     /** @test */
     function can_call_a_route_with_optional_parameters()
     {
-        $this->router = new class extends Bedrock\Routing\Router {
-            public function __construct()
-            {
-                $this->get('/test/{word}/something/{?data}', function ($word, $data = '') {
-                    return "You entered $word and $data";
-                });
-            }
-        };
+        $this->router = new Router;
+        $this->router->get('/test/{word}/something/{?data}', function ($word, $data = '') {
+            return "You entered $word and $data";
+        });
 
         $this->get('/test/foo/something/bar')
             ->see("You entered foo and bar");
